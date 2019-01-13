29da16cf7e4148d58feec2549362c2fc8f1575d9
Dominik Rieder <dominik.rieder@interdiscount.ch>
2019-01-13 16:00:24 +0100

    add checkout template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductOverviewComponent } from './product-overview/product-overview.co
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CartComponent } from './cart/cart.component';
     ProductOverviewComponent,
     LoginComponent,
     RegistrationComponent,
-    CartComponent
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
