dd1f985ef0f66e49ce51f0dae5914cb044e845f4
fabianrindlisbacher <rindlisbacherfabian@gmail.com>
2019-01-28 15:35:44 +0100

    actually added component

---
+++ src/app/components/catalog-admin/catalog-admin.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {Book} from '../../types';
+import {APIService} from '../../services/api.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-catalog-admin',
+  templateUrl: './catalog-admin.component.html',
+  styleUrls: ['./catalog-admin.component.scss']
+})
+export class CatalogAdminComponent implements OnInit {
+  adminForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private apiService: APIService, private router: Router) {
+    this.adminForm = this.createFormGroup(formBuilder);
+  }
+
+  ngOnInit() {
+  }
+
+  createFormGroup(formBuilder: FormBuilder) {
+    return formBuilder.group({
+      title: new FormControl(),
+      subtitle: new FormControl(),
+      isbn: new FormControl(),
+      authors: new FormControl(),
+      description: new FormControl(),
+      imageUrl: new FormControl(),
+      numberOfPages: new FormControl(),
+      price: new FormControl(),
+      publicationYear: new FormControl(),
+      publisher: new FormControl(),
+    });
+  }
+
+  obSubmit() {
+    const book: Book = Object.assign({}, this.adminForm.value);
+    console.log("clicked");
+    this.apiService.addBook(book).subscribe(() => {
+      this.router.navigateByUrl('/products/undefined');
+    });
+  }
+}
